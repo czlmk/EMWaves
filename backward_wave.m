e7413cbe3636f5ed91504a0ae02ab558074b4b04
Vasily Piccone <59125508+Vasily-Piccone@users.noreply.github.com>
2021-01-21 14:33:53 -0500

    Added the movie_forward_and_backward_wave  file.

--- backward_wave.m
+++ backward_wave.m
@@ -1,5 +1,5 @@
 %For a backward-moving wave
-function [vb, i_b] = backward_wave(v0, omega, phi, z0, vp, z, t)
-   vb=@ (z,t) v0*cos(omega*(t+z/vp)+phi)
-   i_b= -vf/z0
+function [vf, i_f] = backward_wave(v0, omega, phi, z0, vp, z, t)
+   vf = @ (z,t) v0*cos(omega*(t-z/vp)+phi);
+   i_f = @ (z,t) -(v0/z0)*cos(omega*(t+z/vp)+phi);
 end
--- movie_forward_and_backward_wave.m
+++ movie_forward_and_backward_wave.m
@@ -3,13 +3,30 @@
 v0 = 1; %Units of volts
 omega = 2*pi*10^(-9); %rad/s
 z0 = 50; %Ohms
+phi = 0; %rads
+vp = 2*10^8; %m/s
+
+t_total = 5; %ns
+z_total = 0.4; %m
+samples = 1000;
+
+% Defining the sampling period for each array
+tstep = t_total/samples; 
+zstep = z_total/samples;
+
+% Defining the space and time arrays
+t = (0:tstep:t_total);
+z = (0:zstep:z_total);
+
+%Create forward and backward waves
+[vf, i_f] = forward_wave(v0, omega, phi, z0, vp, z, t)
 
 for k=1:length(t)
-    plot(z, vf(k,:),’b’); hold on;
-    plot(z, vb(k,:),’r’); hold off;
-    xlabel(‘z [m]’);
-    ylabel(‘v(z,t)[V]’);
-    title(‘instantaneous voltage on a lossless line’); legend(‘v_f(z,t)’,’ v_b(z,t)’);
+    plot(z, vf(k, :),'b'); hold on;
+    plot(z, vb(k, :),'r'); hold off;
+    xlabel('z [m]');
+    ylabel('v(z,t)[V]');
+    title('instantaneous voltage on a lossless line'); legend('v_f(z,t)',' v_b(z,t)');
     axis([0 4 -1 1]);
     M(k)=getframe;
 end;
